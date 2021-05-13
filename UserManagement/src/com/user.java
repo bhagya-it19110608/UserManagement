b38a08f34d6b3269f9797a6a4278ffa1d90510d8
bhagya-it19110608 <bhagyaweerasinghe02@gmail.com>
2021-05-13 17:55:35 +0530

    User database authorized.

--- UserManagement/src/com/user.java
+++ UserManagement/src/com/user.java
@@ -1,7 +1,25 @@
 package com;
 
-
+import java.sql.*; 
 public class user 
 { 
+	//A common method to connect to the DB
+
+	private Connection connect() 
+	 { 
+	 Connection con = null; 
+	 try
+	 { 
+	 Class.forName("com.mysql.jdbc.Driver"); 
+	 
+	 //Provide the correct details: DBServer/DBName,username, password 
+	 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/users", "root", "projectdatabases"); 
+	 } 
+	    catch (Exception e) 
+	    {e.printStackTrace();} 
+	 
+	    return con; 
+	 } 
+
 
 } 
